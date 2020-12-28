9bd6fe638ffc55c58760c0628ecd8fc0c150fab9
xidaokun <xidaokun@elastos.org>
2020-12-28 10:10:05 +0800

    remove processor

--- src/main/java/org/warship/annotation/TestClass.java
+++ src/main/java/org/warship/annotation/TestClass.java
@@ -3,9 +3,6 @@ package org.warship.annotation;
 public class TestClass {
 
 	public static void main(String[] args) {
-		GroovyUtil.invokeMethod("ProcessorHelper.groovy", "create"
-				, "./src/main/java/org/warship/annotation/"
-				, "VersionClass.java"
-				, "aaaaa");
+
 	}
 }
